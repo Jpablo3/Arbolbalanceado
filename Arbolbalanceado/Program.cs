781ab9d44f13b89a25fcbcd58ab2932b3ecbca87
ljuan <ljuan@DESKTOP-41GABFO>
2024-05-31 22:11:55 -0600

    Agregar archivos de proyecto.

---
+++ Arbolbalanceado/Program.cs
@@ -0,0 +1,21 @@
+﻿using Arbolbalanceado.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArbolBalanceado arbol = new ArbolBalanceado();
+
+        
+        arbol.Insertar(10);
+        arbol.Insertar(5);
+        arbol.Insertar(15);
+        arbol.Insertar(3);
+        arbol.Insertar(7);
+
+       
+        Console.WriteLine("Árbol balanceado:");
+        arbol.Mostrar();
+        Console.WriteLine("\nlos nodos han sido insertados correctamente\n");
+    }
+}
---
+++ Arbolbalanceado/Services/Arbolbalaceado.cs
@@ -0,0 +1,61 @@
+﻿using Arbolbalanceado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbolbalanceado.Services
+{
+    public class ArbolBalanceado
+    {
+        public Nodo Raiz;
+
+        public ArbolBalanceado()
+        {
+            Raiz = null;
+        }
+
+        
+        public void Insertar(int valor)
+        {
+            Raiz = Insertar(Raiz, valor);
+        }
+
+      
+        private Nodo Insertar(Nodo nodo, int valor)
+        {
+            if (nodo == null)
+            {
+                nodo = new Nodo(valor);
+            }
+            else if (valor < nodo.Valor)
+            {
+                nodo.Izquierdo = Insertar(nodo.Izquierdo, valor);
+            }
+            else
+            {
+                nodo.Derecho = Insertar(nodo.Derecho, valor);
+            }
+
+            return nodo;
+        }
+
+       
+        public void Mostrar()
+        {
+            Mostrararbol(Raiz);
+        }
+
+       
+        private void Mostrararbol(Nodo nodo)
+        {
+            if (nodo != null)
+            {
+                Mostrararbol(nodo.Izquierdo);
+                Console.Write($"{nodo.Valor} ");
+                Mostrararbol(nodo.Derecho);
+            }
+        }
+    }
+}
